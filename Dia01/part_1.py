285ecf197a97d8b040928d71ccb677e312ef617d
FuzzyGrim <stromed_chariotman@aleeas.com>
2021-12-01 14:46:38 +0100

    cleaning

--- Dia01/part_1.py
+++ Dia01/part_1.py
@@ -1,9 +1,8 @@
-data = open('input.txt', 'r').read().split('\n')
+with open("input.txt") as file:
+    data = file.read().split('\n')
 
 increased = 0
 
 for i in range(0, len(data) - 2):
     if int(data[i+1]) > int(data[i]):
         increased += 1
-
-print(increased)
--- Dia01/part_2.py
+++ Dia01/part_2.py
@@ -1,9 +1,10 @@
-data = open('input.txt', 'r').read().split('\n')
+with open("input.txt") as file:
+    data = file.read().split('\n')
 
 increased = 0
 
 for i in range(0, len(data) - 4):
-    sum_1 = int(data[i]) + int(data[i+1]) + int(data[i+2])
-    sum_2 = int(data[i+1]) + int(data[i+2]) + int(data[i+3])
+    sum_1 = int(data[i])
+    sum_2 = int(data[i+3])
     if sum_2 > sum_1:
         increased += 1
